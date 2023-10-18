92f39544c264ae338bf0662a5d5c921e3ddd8529
kimperria <kimwaweru100@gmail.com>
2023-10-19 00:56:37 +0300

    ch(linters): modify setup
    install linters and compatible packages
    create config files for eslint, prettier, npm and husky
    fix eslint and prettier errors and define initial rules
    create vscode workspace configuration

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,5 +1,3 @@
 export default function Login() {
-  return (
-    <h1>Login page</h1>
-  )
+  return <h1>Login page</h1>;
 }
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,5 +1,3 @@
 export default function Register() {
-  return (
-    <h1>Register page</h1>
-  )
+  return <h1>Register page</h1>;
 }
