a110db97f7b52bd75cb99a9f7cdee8d8fcef4f58
kimperria <kimwaweru100@gmail.com>
2023-11-15 23:11:42 +0300

    ch(utils) add icons and animated button component
    create custom icons component
    add animated button

---
+++ src/common/utils/icons.js
@@ -0,0 +1,7 @@
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+export const icons = {
+  VisibilityIcon,
+  VisibilityOffIcon
+};
